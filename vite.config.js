d18cce16fd45a8dbe54e91d8b741e07b5f25ebf6
nissano <nissano@wix.com>
2024-06-17 12:35:02 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cs-hack-2024/',
+  base: '/cs-hack-2024',
 });
