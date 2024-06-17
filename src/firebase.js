06d0a8ea7bdeb6c067a30c66f5276ee69b97fb5b
nissano <nissano@wix.com>
2024-06-17 08:59:31 +0300

    7-key-env

--- src/firebase.js
+++ src/firebase.js
@@ -5,8 +5,17 @@ Check Firestore section on Installations.md
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+/*
+Environment Variables: These are key-value pairs that can influence the way running processes will behave on a computer. 
+For applications, they provide a way to set configuration and secrets (like API keys) outside of the application code.
+
+.env File: This is a simple text file in which environment variables are defined. 
+It's typically used in development to store configuration settings that shouldn't be hard-coded in the application.
+*/
+
+const apiKey = import.meta.env.API_KEY;
 const firebaseConfig = {
-  apiKey: 'AIzaSyALSyKlFBiIi1Xi95ngF2o3ugSogfmdvMk',
+  apiKey: apiKey,
   authDomain: 'cs-hack-2024.firebaseapp.com',
   projectId: 'cs-hack-2024',
   storageBucket: 'cs-hack-2024.appspot.com',
